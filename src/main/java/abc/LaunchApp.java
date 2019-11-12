78cf9f1397a35140e2e2dc5ae9500223a3e3968f
yelati12 <51700885+yelati12@users.noreply.github.com>
2019-11-12 18:38:50 +0530

    first commit

---
+++ src/main/java/abc/LaunchApp.java
@@ -0,0 +1,18 @@
+package abc;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchApp {
+
+	public static void main(String[] args) {
+		// TODO Au
+		
+ System.setProperty("webdriver.gecko.driver", "D:\\MyLearning\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://127.0.0.1/orangehrm-2.5.0.2/login.php");
+		
+	}
+
+}
