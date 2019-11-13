efca6a38e63ad6f33b992bba291d8dc49c416d12
yelati12 <51700885+yelati12@users.noreply.github.com>
2019-11-13 10:29:20 +0530

    Update LaunchApp.java

--- src/main/java/abc/LaunchApp.java
+++ src/main/java/abc/LaunchApp.java
@@ -8,7 +8,7 @@ public class LaunchApp {
 	public static void main(String[] args) {
 		// TODO Au
 		
- System.setProperty("webdriver.gecko.driver", "D:\\MyLearning\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+ //System.setProperty("webdriver.gecko.driver", "D:\\MyLearning\\geckodriver-v0.26.0-win64\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("http://127.0.0.1/orangehrm-2.5.0.2/login.php");
